6bf1604939921f122dba22f4ba950738b0106004
Sugaya <sugaya324@gmail.com>
2019-09-17 02:09:04 -0700

    commit

--- 0013_RomanToInteger.js
+++ 0013_RomanToInteger.js
@@ -1,31 +1,40 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-  const map = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-  let i = s.length;
-  let result = 0;
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var romanToInt = function(s) {
+//   const map = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000
+//   };
+//   let i = s.length;
+//   let result = 0;
 
-  while (i--) {
-    const curr = map[s.charAt(i)];
-    const prev = map[s.charAt(i - 1)];
+//   while (i--) {
+//     const curr = map[s.charAt(i)];
+//     console.log(`curr is ${curr}`);
 
-    result += curr;
+//     const prev = map[s.charAt(i - 1)];
+//     console.log(`prev is ${prev}\n`);
 
-    if (prev < curr) {
-      result -= prev;
-      i -= 1;
-    }
-  }
+//     result += curr;
 
-  return result;
-};
+//     if (prev < curr) {
+//       result -= prev;
+//       i -= 1;
+//     }
+//   }
+
+//   return result;
+// };
+
+// romanToInt('LVIII');
+// // console.log(romanToInt('III'));
+// // console.log(romanToInt('IV'));
+// // console.log(romanToInt('LVIII'));
+// // console.log(romanToInt('MCMXCIV'));
