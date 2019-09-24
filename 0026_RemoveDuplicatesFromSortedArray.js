b7997ad7fabb9520831d304839d5db33df8e3cda
Sugaya <sugaya324@gmail.com>
2019-09-24 04:37:38 -0700

    commit

--- 0026_RemoveDuplicatesFromSortedArray.js
+++ 0026_RemoveDuplicatesFromSortedArray.js
@@ -5,19 +5,21 @@
 var removeDuplicates = function(nums) {
   // newArray = nums.filter((elem, index, self) => self.indexOf(elem) === index);
   // return newArray.length;
+  if (nums.length === 0) {
+    return 0;
+  }
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
-      // console.log(nums[i], nums[j]);
-      if (i !== j) {
-        if (nums[i] === nums[j]) {
-          nums.splice(j, 1);
-        }
-      }
+    if (nums[i] !== nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
     }
   }
   return nums.length;
 };
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// nums = [1, 1, 1];
+// nums = [0, 0, 0, 0, 0];
+nums = [1, 1, 2];
 // removeDuplicates(nums);
 console.log(removeDuplicates(nums));
