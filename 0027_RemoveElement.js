dd2279d0d776a0a9461a1c0ed095e63442867c23
Sugaya <sugaya324@gmail.com>
2019-09-24 15:00:56 -0700

    commit

--- 0027_RemoveElement.js
+++ 0027_RemoveElement.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  console.log(nums, val);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
 };
+
+nums = [0, 1, 2, 2, 3, 0, 4, 2];
+val = 2;
+console.log(removeElement(nums, val));
