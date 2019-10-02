f33b642a75124016eff19b02f37d64b6f7fff46d
YukiSugaya <sugaya324@gmail.com>
2019-10-02 15:56:08 -0700

    commit

--- 0977_SquaresOfASortedArray.js
+++ 0977_SquaresOfASortedArray.js
@@ -2,4 +2,9 @@
  * @param {number[]} A
  * @return {number[]}
  */
-var sortedSquares = function(A) {};
+var sortedSquares = function(A) {
+  return A.map(x => x ** 2).sort((a, b) => (a < b ? -1 : 1));
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+console.log(sortedSquares([-7, -3, 2, 3, 11]));
