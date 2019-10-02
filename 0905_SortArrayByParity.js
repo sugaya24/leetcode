5e761369dcce4c6c0cdfb8dc84871e6d5f72b00a
YukiSugaya <sugaya324@gmail.com>
2019-10-02 15:35:35 -0700

    commit

--- 0905_SortArrayByParity.js
+++ 0905_SortArrayByParity.js
@@ -2,4 +2,18 @@
  * @param {number[]} A
  * @return {number[]}
  */
-var sortArrayByParity = function(A) {};
+var sortArrayByParity = function(A) {
+  const even_list = [];
+  const odd_list = [];
+  for (let i = 0; i < A.length; i++) {
+    const element = A[i];
+    if (element % 2 == 0) {
+      even_list.push(element);
+    } else {
+      odd_list.push(element);
+    }
+  }
+  return even_list.concat(odd_list);
+};
+
+console.log(sortArrayByParity([3, 1, 2, 4]));
