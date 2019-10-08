c7d932e9b6a8110971ff9e35f1c406c808794360
YukiSugaya <sugaya324@gmail.com>
2019-10-07 19:41:57 -0700

    commit

--- 1217_PlayWithChips.js
+++ 1217_PlayWithChips.js
@@ -2,4 +2,20 @@
  * @param {number[]} chips
  * @return {number}
  */
-var minCostToMoveChips = function(chips) {};
+var minCostToMoveChips = function(chips) {
+  even = 0;
+  odd = 0;
+  chips.forEach(element => {
+    if (element % 2 == 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  });
+  if (even < odd) {
+    return even;
+  }
+  return odd;
+};
+
+// Math.max()
