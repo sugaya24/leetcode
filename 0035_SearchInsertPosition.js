a496700113e20713fbe35da02333ec2ebb64bfc5
YukiSugaya <sugaya324@gmail.com>
2019-10-19 23:57:37 -0700

    simple solution

--- 0035_SearchInsertPosition.js
+++ 0035_SearchInsertPosition.js
@@ -3,4 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {};
+var searchInsert = function(nums, target) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (target <= nums[i]) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // 4
+console.log(searchInsert([1, 3, 5, 6], 0)); // 0
+console.log(searchInsert([], 0)); // 0
+console.log(searchInsert([1], 1)); // 1
