e77f771f59cd9a86cafd48a53eb1a3fbed3dbe47
YukiSugaya <sugaya324@gmail.com>
2019-11-21 17:16:13 -0800

    1. Two Sum

---
+++ 0001_TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return false;
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 22));
