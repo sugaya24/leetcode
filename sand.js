d07d931944e544e957f909e74ad9a06d5e8f10d7
YukiSugaya <sugaya324@gmail.com>
2019-11-21 17:06:14 -0800

    commit

--- sand.js
+++ sand.js
@@ -3,31 +3,25 @@
  * @param {string} s
  * @return {number}
  */
+const romanMap = new Map([
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000]
+]);
 var romanToInt = function(s) {
-  roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  integer = [1, 5, 10, 50, 100, 500, 1000];
   result = 0;
 
-  if (s.length === 1) {
-    return integer[roman.indexOf(s[0])];
-  }
-  arr = s.split('');
-  result += integer[roman.indexOf(s[s.length - 1])];
-  for (let i = arr.length - 1; i > 0; i--) {
-    if (integer[roman.indexOf(s[i])] <= integer[roman.indexOf(s[i - 1])]) {
-      result += integer[roman.indexOf(s[i - 1])];
-    } else if (
-      integer[roman.indexOf(s[i])] > integer[roman.indexOf(s[i - 1])]
-    ) {
-      result -= integer[roman.indexOf(s[i - 1])];
+  result += romanMap.get(s[s.length - 1]);
+  for (let i = s.length - 1; i > 0; i--) {
+    if (romanMap.get(s[i]) <= romanMap.get(s[i - 1])) {
+      result += romanMap.get(s[i - 1]);
+    } else if (romanMap.get(s[i]) > romanMap.get(s[i - 1])) {
+      result -= romanMap.get(s[i - 1]);
     }
   }
   return result;
 };
-
-romanToInt('V');
-romanToInt('III');
-romanToInt('IV');
-romanToInt('IX');
-romanToInt('LVIII');
-romanToInt('MCMXCIV');
