a270475f7a1546ed609aac8720e57b0b1392c409
YukiSugaya <sugaya324@gmail.com>
2019-11-29 18:00:10 -0800

    0821_shortest_distance_to_a_character

---
+++ 0821_shortest_distance_to_a_character.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} S
+ * @param {character} C
+ * @return {number[]}
+ */
+var shortestToChar = function(S, C) {
+  let arr = [];
+  for (let i = 0; i < S.length; i++) {
+    let min = Infinity;
+
+    for (let j = i; j < S.length; j++) {
+      if (S[j] === C) {
+        min = j - i;
+        break;
+      }
+    }
+
+    for (let k = i; k >= 0; k--) {
+      if (S[k] === C && i - k <= min) {
+        min = i - k;
+        break;
+      }
+    }
+    arr.push(min);
+  }
+  return arr;
+};
+
+console.log(shortestToChar('loveleetcode', 'e'));
+console.log(shortestToChar('lovelove', 'l'));
