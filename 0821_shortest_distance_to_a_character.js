826849027f8b2ff2a5bbb9ff9b0e364196f46b0c
YukiSugaya <sugaya324@gmail.com>
2019-12-05 13:48:24 -0800

    Clean: 0821_shortest_distance_to_a_character.js

--- 0821_shortest_distance_to_a_character.js
+++ 0821_shortest_distance_to_a_character.js
@@ -7,14 +7,12 @@ var shortestToChar = function(S, C) {
   let arr = [];
   for (let i = 0; i < S.length; i++) {
     let min = Infinity;
-
     for (let j = i; j < S.length; j++) {
       if (S[j] === C) {
         min = j - i;
         break;
       }
     }
-
     for (let k = i; k >= 0; k--) {
       if (S[k] === C && i - k <= min) {
         min = i - k;
