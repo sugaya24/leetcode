b516dc22e53eac26fef8c66d931219f1650dd4c5
YukiSugaya <sugaya324@gmail.com>
2019-12-05 16:21:31 -0800

    Improve: 0771_jewels_and_stones

--- 0771_jewels_and_stones.js
+++ 0771_jewels_and_stones.js
@@ -5,11 +5,9 @@
  */
 var numJewelsInStones = function(J, S) {
   let count = 0;
-  for (let i = 0; i < J.length; i++) {
-    for (let j = 0; j < S.length; j++) {
-      if (J[i] === S[j]) {
-        count++;
-      }
+  for (let i = 0; i < S.length; i++) {
+    if (J.indexOf(S[i]) >= 0) {
+      count++;
     }
   }
   return count;
