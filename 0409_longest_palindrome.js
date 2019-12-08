c4b9f1614b7f7cf9929489786c64376eebe2640b
YukiSugaya <sugaya324@gmail.com>
2019-12-07 23:42:47 -0800

    Add: 0409_longest_palindrome

---
+++ 0409_longest_palindrome.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+  let result = 0;
+  let isExistOdd = false;
+  const obj = {};
+  for (let i = 0, j = s.length; i < j; i++) {
+    obj[s[i]] = (obj[s[i]] || 0) + 1;
+  }
+  for (const key of Object.keys(obj)) {
+    if (obj[key] % 2 === 1) {
+      result += obj[key] - 1;
+      isExistOdd = true;
+    }
+    if (obj[key] % 2 === 0) {
+      result += obj[key];
+    }
+  }
+  return isExistOdd ? result + 1 : result;
+};
+
+console.log(longestPalindrome('abccccdd')); // 7
+console.log(longestPalindrome('abbcccdddd')); // 9
+console.log(longestPalindrome('aaaabbbb')); // 8
+console.log(longestPalindrome('aaabbbcccc')); // 9
+console.log(longestPalindrome('aaabbbaabb')); // 9
+console.log(
+  longestPalindrome(
+    'civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'
+  )
+); // 983
