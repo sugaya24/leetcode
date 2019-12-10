dba91e9badb36bf48c1f47ea82fbcc96c3a8ad04
YukiSugaya <sugaya324@gmail.com>
2019-12-09 16:57:58 -0800

    Add: 0111_minimum_depth_of_binary_tree

---
+++ 0111_minimum_depth_of_binary_tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+  if (!root) return 0;
+  if (!root.left) return minDepth(root.right) + 1;
+  if (!root.right) return minDepth(root.left) + 1;
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
+
+console.log(minDepth([3, 9, 20, null, null, 15, 7])); // 2
+console.log(minDepth([1, 2])); // 2
