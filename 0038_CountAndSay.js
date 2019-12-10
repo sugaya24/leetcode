b8e4b0c23d5a812c14753bc0db7891f87dc2a645
YukiSugaya <sugaya324@gmail.com>
2019-12-10 09:27:22 -0800

    Change: 0038_CountAndSay (Iterate)

--- 0038_CountAndSay.js
+++ 0038_CountAndSay.js
@@ -3,18 +3,25 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-  if (n === 1) return '1';
-  let prev = countAndSay(n - 1).split('');
-
   let result = '';
-  let lastIndex = -1;
-  for (let i = 0; i < prev.length; i++) {
-    if (prev[i] !== prev[i + 1]) {
-      result += i - lastIndex + prev[i];
-      lastIndex = i;
-    }
+  while (n--) {
+    result = solve(result);
   }
   return result;
+};
+
+const solve = str => {
+  if (str === '') return '1';
+  let solved = '';
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    while (str[i] === str[i + 1]) {
+      count++;
+      i++;
+    }
+    solved += count + str[i];
+  }
+  return solved;
 };
 
 console.log(countAndSay(1)); // 1
