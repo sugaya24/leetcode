d1c88f932895cc839f467ef76e57a201440be733
YukiSugaya <sugaya324@gmail.com>
2020-02-07 18:51:05 -0800

    Mod: 0118_PascalsTriangle

--- 0118_PascalsTriangle.js
+++ 0118_PascalsTriangle.js
@@ -3,20 +3,31 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-  var outer = [];
+  let outer = [];
   for (let i = 0; i < numRows; i++) {
     let inner = [];
-    for (let j = 0; j <= i; j++) {
-      // inner.push(j);
-      if (j === 0 || j === i) {
+    for (let j = 0; j < i + 1; j++) {
+      if (i === 0) {
         inner.push(1);
       } else {
-        inner.push(outer[i - 1][j - 1] + outer[i - 1][j]);
+        if (j === 0 || j === i) {
+          inner.push(1);
+        } else {
+          inner.push(outer[i - 1][j - 1] + outer[i - 1][j]);
+        }
       }
     }
     outer.push(inner);
   }
-  console.log(outer);
+  return outer;
 };
 
-generate(10);
+console.log(generate(5));
+// generate(5);
+// [
+//   [1],
+//   [1,1],
+//  [1,2,1],
+// [1,3,3,1],
+// [1,4,6,4,1]
+// ]
