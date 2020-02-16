ff72774b5fa6ec17440e6b011d70b576c3071236
YukiSugaya <sugaya324@gmail.com>
2020-02-16 02:16:51 -0800

    Mod: 0500_keyboard_row (Set)

--- 0500_keyboard_row.js
+++ 0500_keyboard_row.js
@@ -3,12 +3,22 @@
  * @return {string[]}
  */
 function findWords(words) {
-  return words.filter(
-    (w) =>
-      /^[qwertyuiop]*$/i.test(w) ||
-      /^[asdfghjkl]*$/i.test(w) ||
-      /^[zxcvbnm]*$/i.test(w)
-  );
+  const row1 = new Set('qwertyuiop');
+  const row2 = new Set('asdfghjkl');
+  const row3 = new Set('zxcvbnm');
+
+  const isSameRow = (letters, row) => {
+    return letters.every((letter) => row.has(letter));
+  };
+
+  return words.filter((word) => {
+    const letters = word.toLowerCase().split('');
+    return (
+      isSameRow(letters, row1) ||
+      isSameRow(letters, row2) ||
+      isSameRow(letters, row3)
+    );
+  });
 }
 
 console.log(findWords(['Hello', 'Alaska', 'Dad', 'Peace'])); // ["Alaska", "Dad"]
