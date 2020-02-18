6b8383a5d0e13bdec0ca7197802cc989c9e0b6cb
YukiSugaya <sugaya324@gmail.com>
2020-02-17 20:00:43 -0800

    Add: 1189_maximum_number_of_balloons

---
+++ 1189_maximum_number_of_balloons.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+function maxNumberOfBalloons(text) {
+  let count = 0;
+  const map = new Map();
+  for (const char of text) {
+    map.set(char, (map.get(char) || 0) + 1);
+    if (
+      map.get('b') > 0 &&
+      map.get('a') > 0 &&
+      map.get('n') > 0 &&
+      map.get('l') > 1 &&
+      map.get('o') > 1
+    ) {
+      count++;
+      map.set('b', map.get('b') - 1);
+      map.set('a', map.get('a') - 1);
+      map.set('n', map.get('n') - 1);
+      map.set('l', map.get('l') - 2);
+      map.set('o', map.get('o') - 2);
+    }
+  }
+  return count;
+}
+
+console.log(maxNumberOfBalloons('nlaebolko')); // 1
+console.log(maxNumberOfBalloons('loonbalxballpoon')); // 2
+console.log(maxNumberOfBalloons('leetcode')); // 0
