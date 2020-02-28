028d404af04fa8f966609cf8e3140225fd2f7afc
YukiSugaya <sugaya324@gmail.com>
2020-02-27 16:20:39 -0800

    Add: 141.linked-list-cycle

---
+++ 141.linked-list-cycle.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function hasCycle(head) {
+  if (!head || !head.next) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (fast === slow) return true;
+  }
+  return false;
+}
+// @lc code=end
