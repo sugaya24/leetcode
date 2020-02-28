4135c311a60f2837be2aedbbd1fc4956efe2c5ed
YukiSugaya <sugaya324@gmail.com>
2020-02-28 01:33:22 -0800

    Mod: 121.best-time-to-buy-and-sell-sto… (Kadane's)

--- 121.best-time-to-buy-and-sell-stock.js
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -10,15 +10,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let profit = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+  let benefit = 0;
   for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      if (prices[i] < prices[j] && prices[j] - prices[i] > profit) {
-        profit = prices[j] - prices[i];
-      }
+    if (prices[i] < min) {
+      min = prices[i];
+    } else {
+      benefit = Math.max(benefit, prices[i] - min);
     }
   }
-  return profit;
+  return benefit;
 };
 // @lc code=end
 
