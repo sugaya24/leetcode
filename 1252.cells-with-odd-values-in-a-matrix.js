07a5d5b331c76058b57f79e4b11f723383090e44
YukiSugaya <sugaya324@gmail.com>
2020-02-28 11:14:17 -0800

    Add: 1252.cells-with-odd-values-in-a-matrix

---
+++ 1252.cells-with-odd-values-in-a-matrix.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=1252 lang=javascript
+ *
+ * [1252] Cells with Odd Values in a Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} m
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(n, m, indices) {
+  const matrix = [];
+  for (let i = 0; i < n; i++) {
+    matrix.push([]);
+    for (let j = 0; j < m; j++) {
+      matrix[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < indices.length; i++) {
+    for (let j = 0; j < m; j++) {
+      matrix[indices[i][0]][j]++;
+    }
+    for (let k = 0; k < n; k++) {
+      matrix[k][indices[i][1]]++;
+    }
+  }
+  let oddCount = 0;
+  for (const arr of matrix) {
+    for (const num of arr) {
+      if (num % 2 !== 0) oddCount++;
+    }
+  }
+  return oddCount;
+};
+// @lc code=end
+
+console.log(
+  oddCells(2, 3, [
+    [0, 1],
+    [1, 1]
+  ])
+);
