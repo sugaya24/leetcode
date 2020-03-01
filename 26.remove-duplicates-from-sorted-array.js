5036acf1cd684c4c24309cdd186390def876e395
YukiSugaya <sugaya324@gmail.com>
2020-02-29 22:58:40 -0800

    Add: 26.remove-duplicates-from-sorted-array

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+// @lc code=end
+console.log(removeDuplicates([1, 1, 2]));
