e5f837e46a85b4f0636866fabe55832948d28a73
YukiSugaya <sugaya324@gmail.com>
2020-03-04 17:07:46 -0800

    Add: 617.merge-two-binary-trees

---
+++ 617.merge-two-binary-trees.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=617 lang=javascript
+ *
+ * [617] Merge Two Binary Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+function mergeTrees(t1, t2) {
+  if (!t1) return t2;
+  if (!t2) return t1;
+  t1.val += t2.val;
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
+  return t1;
+}
+// @lc code=end
