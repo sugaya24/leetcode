ee42fa210da1c776668e976e527e6b0c06c92a4b
YukiSugaya <sugaya324@gmail.com>
2020-03-09 15:35:02 -0700

    Add: 1.two-sum

---
+++ 1.two-sum.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+}
+// @lc code=end
+
+console.log(twoSum([2, 7, 11, 15], 9));
