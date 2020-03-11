7b80fe1d787319bba76a562c9495184c765cfd75
YukiSugaya <sugaya324@gmail.com>
2020-03-11 00:55:00 -0700

    Add: 48.rotate-image

---
+++ 48.rotate-image.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=48 lang=javascript
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length / 2; j++) {
+      [matrix[i][j], matrix[i][matrix[0].length - j - 1]] = [
+        matrix[i][matrix[0].length - j - 1],
+        matrix[i][j]
+      ];
+    }
+  }
+  return matrix;
+}
+// @lc code=end
+
+console.log(
+  rotate([
+    [15, 13, 2, 5],
+    [14, 3, 4, 1],
+    [12, 6, 8, 9],
+    [16, 7, 10, 11]
+  ])
+);
