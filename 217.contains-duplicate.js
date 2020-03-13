73430772873ef1c951efa2054ab4fe256b58fd50
YukiSugaya <sugaya324@gmail.com>
2020-03-12 18:50:42 -0700

    Mod: 217.contains-duplicate

--- 217.contains-duplicate.js
+++ 217.contains-duplicate.js
@@ -10,6 +10,15 @@
  * @return {boolean}
  */
 function containsDuplicate(nums) {
-  return new Set(nums).size !== nums.length;
+  const set = new Set();
+  for (const num of nums) {
+    if (set.has(num)) return true;
+    set.add(num);
+  }
+  return false;
 }
 // @lc code=end
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
