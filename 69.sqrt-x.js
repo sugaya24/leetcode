cd44afdbec1414d30ca32fba1e63c2cee92bad4f
YukiSugaya <sugaya324@gmail.com>
2020-03-17 23:59:59 -0800

    69.sqrt-x.js

---
+++ 69.sqrt-x.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=69 lang=javascript
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let left = 1;
+  let right = Math.floor(x / 2) + 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
+// @lc code=end
