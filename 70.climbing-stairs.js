eb0e2f9476fe3e7a9aaf02d55ffaeaf930cf087f
YukiSugaya <sugaya324@gmail.com>
2020-03-18 23:59:59 -0800

    70.climbing-stairs.js

---
+++ 70.climbing-stairs.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const arr = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[arr.length - 1];
+};
+// @lc code=end
