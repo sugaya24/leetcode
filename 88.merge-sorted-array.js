3c68c5c12aed2dade058d70c9c284a6e224897af
YukiSugaya <sugaya324@gmail.com>
2020-03-20 23:59:59 -0800

    88.merge-sorted-array

---
+++ 88.merge-sorted-array.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = 0;
+  let j = 0;
+  nums1.splice(m, n);
+  while (j < nums2.length) {
+    if (nums1[i] === undefined || nums1[i] > nums2[j]) {
+      nums1.splice(i, 0, nums2[j]);
+      i++;
+      j++;
+    } else {
+      i++;
+    }
+  }
+};
+// @lc code=end
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
