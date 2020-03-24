bf53602603829d16eb0ccf24c26522fc70b96fed
YukiSugaya <sugaya324@gmail.com>
2020-03-23 23:59:59 -0800

    101.symmetric-tree

---
+++ 101.symmetric-tree.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = (root) => {
+  return helper(root, root);
+};
+const helper = (p, q) => {
+  if (p === null && q === null) {
+    return true;
+  }
+  if (p === null || q === null) {
+    return false;
+  }
+  return p.val === q.val && helper(p.left, q.right) && helper(p.right, q.left);
+};
+// @lc code=end
