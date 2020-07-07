994f2c7b8887abe895493d69338dd27fec1b645d
YukiSugaya <sugaya324@gmail.com>
2020-07-06 23:59:59 -0800

    590.n-ary-tree-postorder-traversal

---
+++ 1022.sum-of-root-to-leaf-binary-numbers.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1022 lang=javascript
+ *
+ * [1022] Sum of Root To Leaf Binary Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function (root, sum = '') {
+  if (!root) return 0;
+
+  const nextSum = sum + root.val;
+  if (!root.left && !root.right) {
+    return parseInt(nextSum, 2);
+  }
+
+  return sumRootToLeaf(root.left, nextSum) + sumRootToLeaf(root.right, nextSum);
+};
+// @lc code=end
