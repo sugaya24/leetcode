2001cb6958906895b7547327aa229fef5ed08bfb
YukiSugaya <sugaya324@gmail.com>
2021-06-03 14:41:00 -0700

    1512.number-of-good-pairs

--- 1512.number-of-good-pairs.js
+++ 1512.number-of-good-pairs.js
@@ -3,10 +3,15 @@
  * @return {number}
  */
 const numIdenticalPairs = (nums) => {
+  const map = {};
   let count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) count++;
+
+  for (const num of nums) {
+    if (map[num]) {
+      count += map[num];
+      map[num] += 1;
+    } else {
+      map[num] = 1;
     }
   }
   return count;
