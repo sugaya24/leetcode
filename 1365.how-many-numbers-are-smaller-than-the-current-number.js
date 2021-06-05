df8f910edcfbe915c30421dca3689487d898a77b
YukiSugaya <sugaya324@gmail.com>
2021-06-05 14:53:12 -0700

    1365.how-many-numbers-are-smaller-than-the-curr...

--- 1365.how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365.how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,6 +3,13 @@
  * @return {number[]}
  */
 const smallerNumbersThanCurrent = (nums) => {
-  const sorted = [...nums].sort((a, b) => a - b);
-  return nums.map((num) => sorted.indexOf(num));
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] > nums[j]) count++;
+    }
+    res.push(count);
+  }
+  return res;
 };
