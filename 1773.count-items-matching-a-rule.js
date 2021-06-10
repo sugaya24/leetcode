6e3950ec41db4c28b2c3ed4b272a94fbc2c67ed8
YukiSugaya <sugaya324@gmail.com>
2021-06-10 15:36:19 -0700

    1773.count-items-matching-a-rule

--- 1773.count-items-matching-a-rule.js
+++ 1773.count-items-matching-a-rule.js
@@ -5,25 +5,9 @@
  * @return {number}
  */
 function countMatches(items, ruleKey, ruleValue) {
-  let count = 0;
-  let keyIndex = null;
-  switch (ruleKey) {
-    case 'type':
-      keyIndex = 0;
-      break;
-    case 'color':
-      keyIndex = 1;
-      break;
-    case 'name':
-      keyIndex = 2;
-      break;
-    default:
-      break;
-  }
-  for (let i = 0; i < items.length; i++) {
-    if (items[i][keyIndex] === ruleValue) count++;
-  }
-  return count;
+  return items
+    .map(([type, color, name]) => ({ type, color, name }))
+    .filter((obj) => obj[ruleKey] === ruleValue).length;
 }
 
 console.log(
