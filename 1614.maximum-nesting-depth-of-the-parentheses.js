250c31c9463f5446edc5b39c98ee232a1850e3d9
YukiSugaya <sugaya324@gmail.com>
2021-06-11 13:35:18 -0700

    1614.maximum-nesting-depth-of-the-parentheses

--- 1614.maximum-nesting-depth-of-the-parentheses.js
+++ 1614.maximum-nesting-depth-of-the-parentheses.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 function maxDepth(s) {
-  let maxDepth = 0;
-  let curDepth = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(') {
-      maxDepth = Math.max(maxDepth, ++curDepth);
-    } else if (s[i] === ')') {
-      curDepth--;
-    }
-  }
-  return maxDepth;
+  let l = 0;
+  let r = 0;
+  return s.split('').reduce((depth, c) => {
+    if (c === '(') l++;
+    if (c === ')') r++;
+    return Math.max(l - r, depth);
+  }, 0);
 }
 
 console.log(maxDepth('(1+(2*3)+((8)/4))+1'));
