b54093fa7f6b1d04efe6c8086d5e2f2b1eaba41c
YukiSugaya <sugaya324@gmail.com>
2021-06-12 16:13:40 -0700

    1859.sorting-the-sentence

--- 1859.sorting-the-sentence.js
+++ 1859.sorting-the-sentence.js
@@ -6,9 +6,9 @@ function sortSentence(s) {
   const arr = s.split(' ');
   const newArr = [];
   for (const str of arr) {
-    newArr[str[str.length - 1]] = str.slice(0, -1);
+    const idx = str[str.length - 1];
+    newArr[idx - 1] = str.slice(0, -1);
   }
-  newArr.splice(0, 1);
   return newArr.join(' ');
 }
 
