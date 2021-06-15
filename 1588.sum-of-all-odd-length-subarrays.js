5eba308533def027f51bec9d9406f915bc1d32a6
YukiSugaya <sugaya324@gmail.com>
2021-06-14 21:21:28 -0700

    1588.sum-of-all-odd-length-subarrays

--- 1588.sum-of-all-odd-length-subarrays.js
+++ 1588.sum-of-all-odd-length-subarrays.js
@@ -3,10 +3,19 @@
  * @return {number}
  */
 function sumOddLengthSubarrays(arr) {
+  // using sliding window
   let res = 0;
   for (let i = 0; i < arr.length; i += 2) {
-    for (let j = 0; j < arr.length - i; j++) {
-      res += arr.slice(j, j + i + 1).reduce((acc, cur) => acc + cur);
+    debugger;
+    const windowLength = i + 1;
+    let curSum = arr.slice(0, windowLength).reduce((acc, cur) => acc + cur);
+    // add sum of initialWindow to res
+    res += curSum;
+    for (let j = 0; j < arr.length - windowLength; j++) {
+      // update curSum
+      curSum += -arr[j] + arr[j + windowLength];
+      // add curSum to res
+      res += curSum;
     }
   }
   return res;
