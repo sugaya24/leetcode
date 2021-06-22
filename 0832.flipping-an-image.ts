8421ee7c52cdfe30bae7b9fe49df3065106337a6
YukiSugaya <sugaya324@gmail.com>
2021-06-22 06:47:50 -0700

    0832.flipping-an-image

--- 0832.flipping-an-image.ts
+++ 0832.flipping-an-image.ts
@@ -1,18 +1,7 @@
 function flipAndInvertImage(image: number[][]): number[][] {
-  const n = image.length;
-  for (let i = 0; i < n; i++) {
-    // flip the image horizontally first
-    for (let j = 0; j < n / 2; j++) {
-      const tmp = image[i][j];
-      image[i][j] = image[i][n - 1 - j];
-      image[i][n - 1 - j] = tmp;
-    }
-    // and then invert it
-    for (let j = 0; j < n; j++) {
-      console.log(image[i][j]);
-      if (image[i][j] === 0) image[i][j] = 1;
-      else image[i][j] = 0;
-    }
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    image[i] = image[i].map((x) => x ^ 1);
   }
   return image;
 }
