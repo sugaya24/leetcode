cb1a6f392d39b216914098eb29deae6c4fc77389
YukiSugaya <sugaya324@gmail.com>
2021-06-25 16:06:22 -0700

    0024.swap-nodes-in-pairs

--- 0024.swap-nodes-in-pairs.ts
+++ 0024.swap-nodes-in-pairs.ts
@@ -15,21 +15,14 @@ function swapPairs(head: ListNode | null): ListNode | null {
   let dummy = new ListNode(-1);
   dummy.next = head;
   let cur = dummy;
-  // スワップするペアが存在するか確認
-  // その間スワップを続ける
-  while (cur.next !== null && cur.next.next !== null) {
-    // スワップの準備
-    let left = cur.next;
-    let right = cur.next.next;
-    // スワップ処理
-    // 奇数ノードの前を偶数ノードにリンク
-    cur.next = right;
-    // 奇数ノードを次の奇数ノードにリンク
-    left.next = right.next;
-    // 偶数ノードを前の奇数ノードにリンク
-    right.next = left;
-    // スワップ処理が終わり次第、curを次の奇数に移動
-    cur = cur.next.next;
+  while (head && head.next) {
+    let n1 = head;
+    let n2 = head.next;
+    cur.next = n2;
+    n1.next = n2.next;
+    n2.next = n1;
+    cur = n1;
+    head = n1.next;
   }
   return dummy.next;
 }
