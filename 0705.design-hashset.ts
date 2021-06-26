e3aac68e768adca4165a93625f12b0f14a3b88e5
YukiSugaya <sugaya324@gmail.com>
2021-06-25 20:35:41 -0700

    0705.design-hashset

---
+++ 0705.design-hashset.ts
@@ -0,0 +1,27 @@
+class MyHashSet {
+  array: number[] = [];
+  constructor() {
+    this.array = [];
+  }
+
+  add(key: number): void {
+    if (this.array.indexOf(key) === -1) this.array.push(key);
+  }
+
+  remove(key: number): void {
+    const index = this.array.indexOf(key);
+    this.array.splice(index, 1);
+  }
+
+  contains(key: number): boolean {
+    return this.array.indexOf(key) !== -1;
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
