e54ea71137903a1654adb248778798cbfe67df75
YukiSugaya <sugaya324@gmail.com>
2021-08-25 16:56:10 -0700

    20.valid-parentheses

---
+++ 20.valid-parentheses.ts
@@ -0,0 +1,25 @@
+function isValid(s: string): boolean {
+  const arr = s.split('');
+  const stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '(' || arr[i] === '{' || arr[i] === '[') {
+      stack.push(arr[i]);
+    } else {
+      if (
+        (arr[i] === ')' && stack[stack.length - 1] === '(') ||
+        (arr[i] === '}' && stack[stack.length - 1] === '{') ||
+        (arr[i] === ']' && stack[stack.length - 1] === '[')
+      )
+        stack.pop();
+      else return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+console.log(isValid('([)]'));
+console.log(isValid('['));
+console.log(isValid('(])'));
