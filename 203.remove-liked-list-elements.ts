3dbe5d36c40e710a031ab63d57c3ee86eaa27bd1
YukiSugaya <sugaya324@gmail.com>
2021-09-22 16:53:00 -0700

    203.remove-liked-list-elements

---
+++ 203.remove-liked-list-elements.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  if (!head) return head;
+  let dummy = new ListNode();
+  let cur = dummy;
+  dummy.next = head;
+  while (cur.next !== null) {
+    if (cur.next.val === val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
+}
