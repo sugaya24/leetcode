d712be9149498d4030f8000735fdb73dd519ee1e
YukiSugaya <sugaya324@gmail.com>
2021-09-22 19:52:58 -0700

    0234.palindrome-linked-list

--- 0234.palindrome-linked-list.ts
+++ 0234.palindrome-linked-list.ts
@@ -11,24 +11,12 @@
  */
 
 function isPalindrome(head: ListNode | null): boolean {
-  function isPalindrome(head: ListNode | null): boolean {
-    if (head.next === null) return true;
-    let slow = head,
-      fast = head;
-    const stack = [];
-    while (fast.next !== null && fast.next.next !== null) {
-      stack.push(slow.val);
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    // is Even
-    if (fast.next !== null) {
-      stack.push(slow.val);
-    }
-    while (slow.next !== null) {
-      slow = slow.next;
-      if (stack.pop() !== slow.val) return false;
-    }
-    return true;
+  let cur = '',
+    reverse = '';
+  while (head) {
+    cur += head.val;
+    reverse = head.val + reverse;
+    head = head.next;
   }
+  return cur === reverse;
 }
