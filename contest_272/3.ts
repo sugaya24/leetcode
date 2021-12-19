2b46280469b5d737c7ccca255978a5aa9e480acc
YukiSugaya <sugaya324@gmail.com>
2021-12-19 13:32:14 +0900

    contest_272.3

--- contest_272/3.ts
+++ contest_272/3.ts
@@ -1,23 +1,15 @@
 export function getDescentPeriods(prices: number[]): number {
-  // rangeが3の場合3 + 2 + 1
-  // 1づつ下降を続ける限りrange++
-  // 上昇次第、計算していく
-  // 最後が下降で終わる時にどうするか
-  let range = 1,
-    ans = 0;
+  let range: number = 1,
+    ans: number = 0;
   for (let i = 0; i < prices.length - 1; i++) {
     if (prices[i] === prices[i + 1] + 1) {
       range++;
     } else {
-      for (let i = 1; i <= range; i++) {
-        ans += i;
-      }
+      // 数列の和の公式
+      ans += (range * (range + 1)) / 2;
       range = 1;
     }
   }
-  // 上昇で終わった時の処理
-  for (let i = 1; i <= range; i++) {
-    ans += i;
-  }
+  ans += (range * (range + 1)) / 2;
   return ans;
 }
