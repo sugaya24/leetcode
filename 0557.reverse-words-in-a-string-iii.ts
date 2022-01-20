64bfb7c18d4e724185449330d39056981bd711b1
YukiSugaya <sugaya324@gmail.com>
2022-01-20 14:29:53 +0900

    0557.reverse-words-in-a-string-iii

--- 0557.reverse-words-in-a-string-iii.ts
+++ 0557.reverse-words-in-a-string-iii.ts
@@ -1,15 +1,13 @@
 export function reverseWords(s: string): string {
-  const splitS: string[][] = s.split(' ').map((str) => str.split(''));
-  splitS.forEach((arr) => {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left < right) {
-      const tmp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = tmp;
-      left++;
-      right--;
+  let res = '';
+  let word = '';
+  for (const c of s) {
+    if (c === ' ') {
+      res += word + ' ';
+      word = '';
+    } else {
+      word = c + word;
     }
-  });
-  return splitS.map((arr) => arr.join('')).join(' ');
+  }
+  return res + word;
 }
