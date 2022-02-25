22b3983157fe18b2d09f775061b8101e4e973ea6
YukiSugaya <sugaya324@gmail.com>
2022-02-25 16:15:06 +0900

    1475.final-prices-with-a-special-discount-in-a-sho

--- 1475.final-prices-with-a-special-discount-in-a-shop.ts
+++ 1475.final-prices-with-a-special-discount-in-a-shop.ts
@@ -1,17 +1,11 @@
 export function finalPrices(prices: number[]): number[] {
-  const result: number[] = [];
   for (let i = 0; i < prices.length; i++) {
-    let flag = false;
     for (let j = i + 1; j < prices.length; j++) {
-      if (prices[j] <= prices[i]) {
-        result.push(prices[i] - prices[j]);
-        flag = true;
+      if (prices[i] >= prices[j]) {
+        prices[i] = prices[i] - prices[j];
         break;
       }
     }
-    if (!flag) {
-      result.push(prices[i]);
-    }
   }
-  return result;
+  return prices;
 }
