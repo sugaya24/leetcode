5bef49bfd0d9a9da9269a54b91e0a239c07094a8
YukiSugaya <sugaya324@gmail.com>
2022-08-07 04:21:29 +0900

    0035.search-insert-position

--- 0035.search-insert-position.ts
+++ 0035.search-insert-position.ts
@@ -1,12 +1,14 @@
 export function searchInsert(nums: number[], target: number): number {
-  let left = 0;
-  let right = nums.length - 1;
+  let left = 0,
+    right = nums.length - 1;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
-    if (nums[mid] < target) {
-      left = mid + 1;
-    } else {
+    if (nums[mid] === target) {
+      return mid;
+    } else if (target < nums[mid]) {
       right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
   return left;
