a8b170b6892de8e48a6d970cd720af2c18f4ad66
YukiSugaya <sugaya324@gmail.com>
2022-08-07 01:18:02 +0900

    0704.binary-search

--- 0704.binary-search.test.ts
+++ 0704.binary-search.test.ts
@@ -1,11 +1,11 @@
-import search from './0704.binary-search';
+import { search } from "./0704.binary-search";
 
-test('0', () => {
+test("0", () => {
   expect(search([-1, 0, 3, 5, 9, 12], 9)).toBe(4);
 });
-test('1', () => {
+test("1", () => {
   expect(search([-1, 0, 3, 5, 9, 12], 2)).toBe(-1);
 });
-test('2', () => {
+test("2", () => {
   expect(search([5], 5)).toBe(0);
 });
--- 0704.binary-search.ts
+++ 0704.binary-search.ts
@@ -1,20 +1,15 @@
-function search(nums: number[], target: number): number {
+export function search(nums: number[], target: number): number {
   let left = 0,
     right = nums.length - 1;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
       return mid;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
     } else if (nums[mid] < target) {
       left = mid + 1;
-    } else if (nums[mid] > target) {
-      right = mid - 1;
     }
   }
   return -1;
 }
-
-export default search;
-
-// search([-1, 0, 3, 5, 9, 12], 9);
-// search([5], 5);
