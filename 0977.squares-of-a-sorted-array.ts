6d143afe09c8276194554e01e8efbad462a4d6ff
YukiSugaya <sugaya324@gmail.com>
2022-08-08 02:07:15 +0900

    0977.squares-of-a-sorted-array

--- 0977.squares-of-a-sorted-array.ts
+++ 0977.squares-of-a-sorted-array.ts
@@ -1,3 +1,15 @@
 export function sortedSquares(nums: number[]): number[] {
-  return nums.map((n) => Math.pow(n, 2)).sort((a, b) => a - b);
+  const ans: number[] = [];
+  let left = 0,
+    right = nums.length - 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (Math.pow(nums[left], 2) <= Math.pow(nums[right], 2)) {
+      ans.unshift(Math.pow(nums[right], 2));
+      right--;
+    } else {
+      ans.unshift(Math.pow(nums[left], 2));
+      left++;
+    }
+  }
+  return ans;
 }
