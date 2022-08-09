6f2799e818d550372b1f645a979a409fa63a1ace
YukiSugaya <sugaya324@gmail.com>
2022-08-09 15:32:46 +0900

    0283.move-zeros

--- 0283.move-zeros.ts
+++ 0283.move-zeros.ts
@@ -2,20 +2,11 @@
  Do not return anything, modify nums in-place instead.
  */
 export function moveZeroes(nums: number[]): void {
-  if (nums.indexOf(0) < 0) {
-    return;
-  }
-  let slow = 0,
-    fast = 0;
-  for (let i = 0; i < nums.length; i++) {
+  for (let lastNonZeroFoundAt = 0, i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      const tmp = nums[fast];
-      nums[fast] = nums[slow];
-      nums[slow] = tmp;
-      slow++;
-      fast = i + 1;
-    } else {
-      fast++;
+      const tmp = nums[lastNonZeroFoundAt];
+      nums[lastNonZeroFoundAt++] = nums[i];
+      nums[i] = tmp;
     }
   }
 }
