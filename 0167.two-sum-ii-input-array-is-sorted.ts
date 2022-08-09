4484f980b75863365d1215248d30219b825a0ed3
YukiSugaya <sugaya324@gmail.com>
2022-08-09 17:37:34 +0900

    0167.two-sum-ii-input-array-is-sorted

--- 0167.two-sum-ii-input-array-is-sorted.ts
+++ 0167.two-sum-ii-input-array-is-sorted.ts
@@ -1,15 +1,19 @@
 export function twoSum(numbers: number[], target: number): number[] {
+  // binary search
   let left = 0,
     right = numbers.length - 1;
-  while (left < right) {
-    const sum = numbers[left] + numbers[right];
-    if (sum === target) {
-      break;
-    } else if (sum < target) {
-      left++;
-    } else {
-      right--;
+  for (let i = 0; i < numbers.length; i++) {
+    left = i + 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (numbers[i] + numbers[mid] === target) {
+        return [i + 1, mid + 1];
+      } else if (numbers[i] + numbers[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
-  return [left + 1, right + 1];
+  return [];
 }
