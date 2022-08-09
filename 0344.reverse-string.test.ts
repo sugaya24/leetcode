56ecdacdeb83f62e7c65e11b007956233cbcc8c5
YukiSugaya <sugaya324@gmail.com>
2022-08-10 06:37:35 +0900

    0344.reverse-string

--- 0344.reverse-string.test.ts
+++ 0344.reverse-string.test.ts
@@ -14,8 +14,78 @@ test('1', () => {
   const expectedArray = ['h', 'a', 'n', 'n', 'a', 'H'];
   const s = ['H', 'a', 'n', 'n', 'a', 'h'];
   reverseString(s);
+  expect(s).toEqual(expectedArray);
   s.forEach((str, i) => {
     expect(str).toBe(expectedArray[i]);
   });
   expect(s.length).toBe(expectedArray.length);
+});
+
+test('2', () => {
+  const expectedArray = [
+    'a',
+    'm',
+    'a',
+    'n',
+    'a',
+    'P',
+    ' ',
+    ':',
+    'l',
+    'a',
+    'n',
+    'a',
+    'c',
+    ' ',
+    'a',
+    ' ',
+    ',',
+    'n',
+    'a',
+    'l',
+    'p',
+    ' ',
+    'a',
+    ' ',
+    ',',
+    'n',
+    'a',
+    'm',
+    ' ',
+    'A',
+  ];
+  const s = [
+    'A',
+    ' ',
+    'm',
+    'a',
+    'n',
+    ',',
+    ' ',
+    'a',
+    ' ',
+    'p',
+    'l',
+    'a',
+    'n',
+    ',',
+    ' ',
+    'a',
+    ' ',
+    'c',
+    'a',
+    'n',
+    'a',
+    'l',
+    ':',
+    ' ',
+    'P',
+    'a',
+    'n',
+    'a',
+    'm',
+    'a',
+  ];
+  reverseString(s);
+  expect(s).toEqual(expectedArray);
 });
--- 0344.reverse-string.ts
+++ 0344.reverse-string.ts
@@ -5,9 +5,7 @@ export function reverseString(s: string[]): void {
   let left = 0;
   let right = s.length - 1;
   while (left < right) {
-    const tmp = s[left];
-    s[left] = s[right];
-    s[right] = tmp;
+    [s[left], s[right]] = [s[right], s[left]];
     left++;
     right--;
   }
