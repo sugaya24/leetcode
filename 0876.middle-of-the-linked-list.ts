b49b175ecb58432499fc6dc7acd18fc490e6461b
YukiSugaya <sugaya324@gmail.com>
2022-08-11 06:15:46 +0900

    0876.middle-of-the-linked-list

--- 0876.middle-of-the-linked-list.ts
+++ 0876.middle-of-the-linked-list.ts
@@ -1,21 +1,26 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 function middleNode(head: ListNode | null): ListNode | null {
-  let slow = head,
-    fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
+  if (!head?.next) {
+    return head;
+  }
+  let slow = head;
+  let fast = head;
+  while (fast?.next && fast.next?.next) {
+    slow = slow?.next!;
     fast = fast.next.next;
   }
-  return slow;
+  if (!fast?.next) {
+    return slow;
+  } else {
+    return slow?.next;
+  }
 }
