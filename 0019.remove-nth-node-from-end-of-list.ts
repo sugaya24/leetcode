b75fc67d88230f943bd2c0973eb0777709aba169
YukiSugaya <sugaya324@gmail.com>
2022-08-11 19:25:28 +0900

    0019.remove-nth-node-from-end-of-list

--- 0019.remove-nth-node-from-end-of-list.ts
+++ 0019.remove-nth-node-from-end-of-list.ts
@@ -1,30 +1,40 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+// Definition for singly-linked list.
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-  let copy = head;
-  let ans = head;
-  let count = 0;
-  while (copy) {
-    copy = copy.next;
-    count++;
+  if (!head) {
+    return null;
   }
-  if (count === n) return head.next;
-  while (count--) {
-    if (count === n) {
-      head.next = head.next.next;
-      break;
+  if (!head.next) {
+    return head.next;
+  }
+  let len = 1;
+  let node = head;
+  while (node.next) {
+    node = node.next;
+    len++;
+  }
+  if (len - n === 0) {
+    return head.next;
+  }
+
+  let dummy = new ListNode();
+  dummy.next = head;
+  let i = 1;
+  while (head.next) {
+    if (i === len - n) {
+      head.next = head.next?.next;
+    } else {
+      head = head?.next;
     }
-    head = head.next;
+    i++;
   }
-  return ans;
+  return dummy.next;
 }
