9ea6534a3827c76ea2297390c54625ad525ee4e9
YukiSugaya <sugaya324@gmail.com>
2022-11-04 03:13:36 +0900

    0033.search-in-rotated-sorted-array

--- 0033.search-in-rotated-sorted-array.ts
+++ 0033.search-in-rotated-sorted-array.ts
@@ -1,40 +1,25 @@
 function search(nums: number[], target: number): number {
-  if (nums.length === 1) return nums.indexOf(target);
-  if (target < nums[0] && nums[nums.length - 1] < target) return -1;
-
   let left = 0;
   let right = nums.length - 1;
-  let bottom = 0;
-  if (nums[left] > nums[right]) {
-    while (left < right) {
-      const mid = Math.floor((left + right) / 2);
-      if (nums[mid] > nums[right]) {
-        left = mid + 1;
-        bottom = mid + 1;
-      } else {
-        right = mid;
-        bottom = mid;
-      }
-    }
-  }
-
-  if (nums[0] <= target && nums[nums.length - 1] < target) {
-    left = 0;
-    right = bottom - 1;
-  } else {
-    left = bottom;
-    right = nums.length - 1;
-  }
 
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
       return mid;
     }
-    if (nums[mid] < target) {
-      left = mid + 1;
+
+    if (nums[left] <= nums[mid]) {
+      if (target > nums[mid] || target < nums[left]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     } else {
-      right = mid - 1;
+      if (target < nums[mid] || target > nums[right]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     }
   }
 
