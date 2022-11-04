6b23b3bf3dc0b30b316c92e3a4950a42da0eb1b4
YukiSugaya <sugaya324@gmail.com>
2022-11-04 21:00:15 +0900

    0153.find-minimum-in-rotated-sorted-array

--- 0153.find-minimum-in-rotated-sorted-array.ts
+++ 0153.find-minimum-in-rotated-sorted-array.ts
@@ -1,21 +1,12 @@
 function findMin(nums: number[]): number {
-  if (nums[0] < nums[nums.length - 1]) return nums[0];
   let l = 0;
   let r = nums.length - 1;
   while (l < r) {
     const mid = Math.floor(l + (r - l) / 2);
-    if (nums[l] < nums[mid]) {
-      if (nums[mid] < nums[r]) {
-        r = mid;
-      } else {
-        l = mid + 1;
-      }
+    if (nums[mid] > nums[r]) {
+      l = mid + 1;
     } else {
-      if (nums[mid] > nums[r]) {
-        l = mid + 1;
-      } else {
-        r = mid;
-      }
+      r = mid;
     }
   }
   return nums[l];
