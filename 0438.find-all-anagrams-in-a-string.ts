098359f5781faea153c40b39460cbc57639ce3b3
YukiSugaya <sugaya324@gmail.com>
2022-11-07 05:57:50 +0900

    0438.find-all-anagrams-in-a-string

--- 0438.find-all-anagrams-in-a-string.ts
+++ 0438.find-all-anagrams-in-a-string.ts
@@ -1,41 +1,34 @@
 function findAnagrams(s: string, p: string): number[] {
-  if (s.length < p.length) return [];
+  let sLen = s.length;
+  let pLen = p.length;
+  const res: number[] = [];
 
-  const map = new Map();
-  for (let i = 0; i < p.length; i++) {
-    map.set(p.charAt(i), map.get(p.charAt(i)) + 1 || 1);
+  let sArr = new Array(26).fill(0);
+  let pArr = new Array(26).fill(0);
+
+  for (let i = 0; i < pLen; i++) {
+    sArr[s.charCodeAt(i) - 97]++;
+    pArr[p.charCodeAt(i) - 97]++;
   }
 
-  let res: number[] = [];
-  const curMap = new Map();
-  let slow = 0;
-  let fast = 0;
-  while (fast < s.length) {
-    if (fast < p.length - 1) {
-      curMap.set(s.charAt(fast), curMap.get(s.charAt(fast)) + 1 || 1);
-    } else {
-      curMap.set(s.charAt(fast), curMap.get(s.charAt(fast)) + 1 || 1);
-
-      let isValid = true;
-      for (const [key, value] of map) {
-        if (curMap.get(key) !== value) {
-          isValid = false;
-          break;
-        }
-      }
-      if (isValid) res.push(slow);
-
-      if (curMap.get(s.charAt(slow)) <= 1) {
-        curMap.delete(s.charAt(slow));
-      } else {
-        curMap.set(s.charAt(slow), curMap.get(s.charAt(slow)) - 1);
-      }
-
-      slow++;
+  for (let i = 0; i < sLen; i++) {
+    if (isAnagram(sArr, pArr)) {
+      res.push(i);
     }
-    fast++;
+    sArr[s.charCodeAt(i) - 97]--;
+    sArr[s.charCodeAt(i + pLen) - 97]++;
   }
+
   return res;
+}
+
+function isAnagram(arr1: number[], arr2: number[]) {
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export default findAnagrams;
