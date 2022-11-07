24cc6d376b0273b981bfce6f179929a4c8df0d14
YukiSugaya <sugaya324@gmail.com>
2022-11-08 03:15:57 +0900

    0200.number-of-islands

---
+++ 0200.number-of-islands.ts
@@ -0,0 +1,32 @@
+function numIslands(grid: string[][]): number {
+  let count = 0;
+  function dfs(grid: string[][], i: number, j: number) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[i].length ||
+      grid[i][j] === '0'
+    ) {
+      return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        count++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+
+  return count;
+}
+
+export default numIslands;
