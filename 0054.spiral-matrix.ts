507471b6a8bae81d5ccdf26fbc94b634aa65efc7
YukiSugaya <sugaya324@gmail.com>
2022-12-11 15:20:34 +0900

    0054.spiral-matrix

--- 0054.spiral-matrix.ts
+++ 0054.spiral-matrix.ts
@@ -1,16 +1,28 @@
 function spiralOrder(matrix: number[][]): number[] {
   const output: number[] = [];
-  while (matrix.length) {
-    const fl = matrix.shift() || [];
-    output.push(...fl);
-    for (const arr of matrix) {
-      const n = arr.pop();
-      if (n) {
-        output.push(n);
-        arr.reverse();
+  let rowStart = 0;
+  let colStart = 0;
+  let rowEnd = matrix.length - 1;
+  let colEnd = matrix[0].length - 1;
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    for (let i = colStart; i <= colEnd; i++) {
+      output.push(matrix[rowStart][i]);
+    }
+    for (let i = colStart + 1; i <= rowEnd; i++) {
+      output.push(matrix[i][colEnd]);
+    }
+    if (rowStart < rowEnd && colStart < colEnd) {
+      for (let i = colEnd - 1; i >= colStart; i--) {
+        output.push(matrix[rowEnd][i]);
+      }
+      for (let i = rowEnd - 1; i > rowStart; i--) {
+        output.push(matrix[i][colStart]);
       }
     }
-    matrix.reverse();
+    rowStart++;
+    colStart++;
+    rowEnd--;
+    colEnd--;
   }
   return output;
 }
