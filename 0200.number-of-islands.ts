b20ab9e71989fa836e0c8cbe21cb187e6d5716f1
YukiSugaya <sugaya324@gmail.com>
2022-12-30 00:31:59 +0900

    0200.number-of-islands

--- 0200.number-of-islands.ts
+++ 0200.number-of-islands.ts
@@ -1,32 +1,27 @@
 function numIslands(grid: string[][]): number {
+  const H = grid.length;
+  const W = H && grid[0].length;
   let count = 0;
-  function dfs(grid: string[][], i: number, j: number) {
-    if (
-      i < 0 ||
-      i >= grid.length ||
-      j < 0 ||
-      j >= grid[i].length ||
-      grid[i][j] === '0'
-    ) {
-      return;
-    }
-    grid[i][j] = '0';
-    dfs(grid, i + 1, j);
-    dfs(grid, i - 1, j);
-    dfs(grid, i, j + 1);
-    dfs(grid, i, j - 1);
-  }
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] === '1') {
-        count++;
-        dfs(grid, i, j);
-      }
+  for (let r = 0; r < H; r++) {
+    for (let c = 0; c < W; c++) {
+      if (grid[r][c] === '0') continue;
+      count++;
+      dfs(r, c);
     }
   }
-
   return count;
+
+  function dfs(r: number, c: number) {
+    if (r < 0 || c < 0 || r === H || c === W) return;
+    if (grid[r][c] === '0') return;
+
+    grid[r][c] = '0';
+    dfs(r - 1, c);
+    dfs(r + 1, c);
+    dfs(r, c - 1);
+    dfs(r, c + 1);
+  }
 }
 
 export default numIslands;
