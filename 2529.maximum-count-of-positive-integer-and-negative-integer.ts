befcefe89984f316e6b617c81ae2c9442ef9399e
YukiSugaya <sugaya324@gmail.com>
2023-01-10 01:31:51 +0900

    2529.maximum-count-of-positive-integer-and-negativ

--- 2529.maximum-count-of-positive-integer-and-negative-integer.ts
+++ 2529.maximum-count-of-positive-integer-and-negative-integer.ts
@@ -1,14 +1,36 @@
 function maximumCount(nums: number[]): number {
-  let neg = 0;
-  let pos = 0;
-  for (const num of nums) {
-    if (num < 0) {
-      neg++;
-    } else if (num > 0) {
-      pos++;
+  console.log(upper_bound(nums), lower_bound(nums));
+  return Math.max(upper_bound(nums), lower_bound(nums));
+}
+
+function upper_bound(nums: number[]) {
+  if (nums[0] >= 0) return 0;
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.ceil((left + right) / 2);
+    if (nums[mid] < 0) {
+      left = mid;
+    } else {
+      right = mid - 1;
     }
   }
-  return Math.max(neg, pos);
+  return left + 1;
+}
+
+function lower_bound(nums: number[]) {
+  if (nums[nums.length - 1] <= 0) return 0;
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > 0) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return nums.length - left;
 }
 
 export default maximumCount;
