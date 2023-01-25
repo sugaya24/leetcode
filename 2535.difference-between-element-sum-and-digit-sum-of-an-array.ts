e8c20a0f394b3404b3618c0c1d264bae8fe82489
YukiSugaya <sugaya324@gmail.com>
2023-01-26 00:25:54 +0900

    2535.difference-between-element-sum-and-digit-sum-

--- 2535.difference-between-element-sum-and-digit-sum-of-an-array.ts
+++ 2535.difference-between-element-sum-and-digit-sum-of-an-array.ts
@@ -2,7 +2,7 @@ function differenceOfSum(nums: number[]): number {
   const elementSum = nums.reduce((acc, cur) => acc + cur, 0);
   let digitSum = 0;
   for (let num of nums) {
-    while (num >= 1) {
+    while (num > 0) {
       digitSum += num % 10;
       num = Math.floor(num / 10);
     }
