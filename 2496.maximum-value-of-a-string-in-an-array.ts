4b6f2b7db8a38774650d1b53e19c92ee1f70b928
YukiSugaya <sugaya324@gmail.com>
2023-01-25 18:04:57 +0900

    2496.maximum-value-of-a-string-in-an-array

--- 2496.maximum-value-of-a-string-in-an-array.ts
+++ 2496.maximum-value-of-a-string-in-an-array.ts
@@ -1,15 +1,11 @@
 function maximumValue(strs: string[]): number {
-  const nums = '0123456789';
   let max = 0;
   for (const str of strs) {
-    let isNum = true;
-    for (const c of str) {
-      if (!nums.includes(c)) {
-        isNum = false;
-        break;
-      }
+    if (isNaN(Number(str))) {
+      max = Math.max(max, str.length);
+    } else {
+      max = Math.max(max, Number(str));
     }
-    max = Math.max(max, isNum ? Number(str) : str.length);
   }
   return max;
 }
