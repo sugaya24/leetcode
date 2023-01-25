043a334cea2bcdc190155658143bbbf5f838b36a
YukiSugaya <sugaya324@gmail.com>
2023-01-25 19:44:28 +0900

    2506.count-pairs-of-similar-strings

--- 2506.count-pairs-of-similar-strings.ts
+++ 2506.count-pairs-of-similar-strings.ts
@@ -1,27 +1,14 @@
 function similarPairs(words: string[]): number {
+  const arr = words.map((word) => [...new Set(word)].sort().join());
   let count = 0;
   for (let i = 0; i < words.length - 1; i++) {
     for (let j = i + 1; j < words.length; j++) {
-      if (isSimilarWords(words[i], words[j])) {
+      if (arr[i] === arr[j]) {
         count++;
       }
     }
   }
   return count;
-}
-
-function isSimilarWords(word1: string, word2: string): boolean {
-  const set1 = new Set(word1.split(''));
-  const set2 = new Set(word2.split(''));
-  if (set1.size !== set2.size) {
-    return false;
-  }
-  for (const c of set1) {
-    if (!set2.has(c)) {
-      return false;
-    }
-  }
-  return true;
 }
 
 export default similarPairs;
