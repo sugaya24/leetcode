83f714df5fbd6a8a708f3d98675cbc5c7c3bdc10
YukiSugaya <sugaya324@gmail.com>
2023-02-18 18:14:42 -0800

    0011.container-with-most-water

--- 0011.container-with-most-water.ts
+++ 0011.container-with-most-water.ts
@@ -1,17 +1,17 @@
 function maxArea(height: number[]): number {
-  let left = 0;
-  let right = height.length - 1;
-  let ans = 0;
-  while (left < right) {
-    const lowerHight = Math.min(height[left], height[right]);
-    ans = Math.max(lowerHight * (right - left), ans);
-    if (height[left] < height[right]) {
-      left++;
+  let l = 0;
+  let r = height.length - 1;
+  let max = 0;
+  while (l < r) {
+    const dist = r - l;
+    max = Math.max(max, dist * Math.min(height[l], height[r]));
+    if (height[l] < height[r]) {
+      l++;
     } else {
-      right--;
+      r--;
     }
   }
-  return ans;
+  return max;
 }
 
 export default maxArea;
