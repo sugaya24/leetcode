d6c5f970f783e9d444474aade8e6a05595678b3a
YukiSugaya <sugaya324@gmail.com>
2023-02-21 05:51:02 -0800

    0049.group-anagrams

--- 0049.group-anagrams.ts
+++ 0049.group-anagrams.ts
@@ -1,13 +1,12 @@
 function groupAnagrams(strs: string[]): string[][] {
-  const map: any = {};
+  const map: Map<string, string[]> = new Map();
   for (const str of strs) {
-    const key = [...str].sort().join('');
-    if (!map[key]) {
-      map[key] = [];
-    }
-    map[key].push(str);
+    const sortedStr = str.split('').sort().join();
+    const values = map.get(sortedStr) || [];
+    values.push(str);
+    map.set(sortedStr, values);
   }
-  return Object.values(map);
+  return [...map.values()];
 }
 
 export default groupAnagrams;
